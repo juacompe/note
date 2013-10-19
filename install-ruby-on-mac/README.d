205750d90d4d045180a52f3d377efcfb63413316
juacompe <juacompe@gmail.com>
2013-10-19 16:47:45 +0700

    add install-ruby-on-mac/

---
+++ install-ruby-on-mac/README.d
@@ -0,0 +1,23 @@
+Install Xcode Command Line Tool
+----
+
+1. From App Store, you can download and install Xcode for free.
+1. Open Xcode and follow menu Xcode > Preferences.. > Downloads > Components > Command Line Tools 
+1. Install Command Line Tools
+
+Install RVM
+----
+
+```
+\curl -L https://get.rvm.io | bash
+```
+
+ref: <https://rvm.io/rvm/install>
+
+Install Ruby
+----
+
+```
+rvm install 2.0
+```
+
