114b2c028017628e35ae1eac03733ef72a05e4f4
juacompe <juacompe@gmail.com>
2018-11-26 11:47:09 +0700

    Upgrade to python3

--- scripts/Autologin.py
+++ scripts/Autologin.py
@@ -1,13 +1,12 @@
-from HTMLParser import HTMLParser
-import httplib
+from html.parser import HTMLParser
+from http import client as httplib
 import urllib
 import ssl
 
 #https://10.0.127.14/
+username="oschokchai"
+password="Nov2018!"
 # Reset Password https://myid.dtac.co.th/showLogin.cc
-
-username="username"
-password="secret"
 
 
 # Authentication choices -- NOT SUPPORT "Specific Sign-on"
@@ -53,38 +52,38 @@ class Parser(HTMLParser):
 			self.result = data
 
 # Get request the Login form
-print "Connecting authen server ..."
+print("Connecting authen server ...")
 conn = httplib.HTTPSConnection(loginIP)
 conn.request("GET","/")
 res = conn.getresponse()
 data = res.read()
 parser = Parser()
-parser.feed(data)
+parser.feed(str(data))
 input_id = parser.input_id
 
 # Post Username
 body = "ID=" + input_id + "&STATE=1&DATA=" + username
 header["Content-Length"] = len(body)
-print "sending Username ..."
+print("sending Username ...")
 conn.request("POST","/",body,header)
 res = conn.getresponse()
 data = res.read()
 parser = Parser()
-parser.feed(data)
+parser.feed(str(data))
 input_id = parser.input_id
 response_text = parser.result
 
 if response_text.find("Unable to activate") == -1:
 	# Post Password
-	escapePassword = urllib.quote(password)
+	escapePassword = urllib.parse.quote(password)
 	body = "ID=" + input_id + "&STATE=2&DATA=" + escapePassword
 	header["Content-Length"] = len(body)
-	print "sending Password ..."
+	print("sending Password ...")
 	conn.request("POST","/",body,header)
 	res = conn.getresponse()
 	data = res.read()
 	parser = Parser()
-	parser.feed(data)
+	parser.feed(str(data))
 	input_id = parser.input_id
 	response_text = parser.result
 	# If Password is correct
@@ -92,17 +91,17 @@ if response_text.find("Unable to activate") == -1:
 		# Post Authen type
 		body = "ID=" + input_id + "&STATE=3&DATA=" + str(selected_choice)
 		header["Content-Length"] = len(body)
-		print "authen with " + authen_choices[selected_choice - 1] + " ..."
+		print("authen with " + authen_choices[selected_choice - 1] + " ...")
 		conn.request("POST","/",body,header)
 		res = conn.getresponse()
 		data = res.read()
 		parser = Parser()
-		parser.feed(data)
-		print parser.result
+		parser.feed(str(data))
+		print(parser.result)
 	else:
-		print response_text
+		print(response_text)
 else:
-	print response_text + "Please check your username"
+	print(response_text + "Please check your username")
 
 import platform
 plat = platform.system()
